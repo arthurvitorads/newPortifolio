d2238fe6cf74f0395278f7a5d4a6ef94428016b6
Arthur <arhturvitorsilvio@gmail.com>
2025-06-30 17:21:17 -0300

    feat: inicando o header do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
-  <p>test</p>
+  <section class="header">
+    <navComponent/>
+  </section>
 </template>
 
-<script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import navComponent from './components/navComponent.vue'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
+    navComponent: navComponent
   }
-}
+});
 </script>
 
 <style>
+
+* {
+  margin: 0px;
+  padding: 0px;
+}
+
 </style>
---
+++ src/components/navComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="navegacao">
+        <a href="">Arthur Vitor</a>
+        <div class="list">
+            <nav class="navegacao-lista">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+            <div class="download-cv">
+                <a href="">Baixar CV</a>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+    name: 'navComponent'
+})
+
+</script>
+
+<style>
+.navegacao {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: #ccc;
+}
+</style>
